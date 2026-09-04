dec6b0e73c43cf9069153785188917f9eab12c26
konovalovm7k-cloud <konovalov.m7k@gmail.com>
2026-09-04 23:45:55 +0300

    feat: launch FitLife 2.0 modern UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'app/app.dart';
+import 'modern_app.dart';
 
-void main() => runApp(const FitLifeApp());
+void main() => runApp(const ModernFitLifeApp());
